b82cf0ec0c198f37e7179a7c92c4f20d866e835a
Stian Howard <stianhoward@gmail.com>
2018-05-18 12:38:35 -0600

    Added Vector Plotting function, and minor changes.

--- lc_turbulence/plot.py
+++ lc_turbulence/plot.py
@@ -12,11 +12,13 @@ import argparse
 import csv
 import pylab as py
 import scipy
-f1=1000     # Not sure what this is. Needs investigation
-mp=.0041033 # mm per pixel
+import sys
+FRAME_RATE= 8000        # FrameRate
+PIXEL_SIZE=.0044     # mm per pixel
 
 def main():
     base_path = '/media/stian/Evan Dutch/Turbulence/2018-05-17/'
+
     # Determine films in path
     films = glob.glob(base_path + 'Film[1-9]')
     for film in films:
@@ -27,11 +29,17 @@ def main():
             save_path = film + '/racetest' + matrix.strip()[-16] + '.png'
 
             try:
-                rawData = pd.read_csv(open_path) # Import a data set
+                raw_data = pd.read_csv(open_path)        # Import a data set
                 location = loadLocation(film + matrix.strip()[-16] + '/position.txt')
-                scatter(rawData, location, save_path) # Create and save Scatter plot of data
+                scatter(raw_data, location, save_path)   # Create and save Scatter plot of data
+                try:
+                    vector(raw_data, location, save_path)    # Create the vector graph
+                except:
+                    print("Unexpected error:", sys.exc_info())
+                    print("Failed to produce plot")
             except:
-                print("Failed to open data for: " + open_path)
+                print("Unexpected error:", sys.exc_info())
+                print("Failed to open and process data for: " + open_path)
 
 
 
@@ -45,7 +53,7 @@ def loadLocation(filePath):
     try:
         file = open(filePath)
         location = file.read()
-        location = location.strip() + ': '
+        location = location.strip()
         file.close()
     except:
         location = ''
@@ -55,27 +63,61 @@ def loadLocation(filePath):
     return location
 
 
-def scatter(rawData, location, save_path):
-        yslice1 = rawData.astype({'x':'int','y':'int'}) #set x and y values to integers
+def scatter(raw_data, location, save_path):
+    yslice1 = raw_data.astype({'x':'int','y':'int'}) #set x and y values to integers
 
-        yslice1['x']=yslice1['x'].apply(lambda x: custom_round(x, base=10))
-        ygrouped1=yslice1.groupby(['x'],as_index=False).agg({'dr':['mean','std']})
-        ygrouped1.columns=['x','dr','std']
-        y1=ygrouped1.fillna(0) #replace NaNs with 0's
+    yslice1['x']=yslice1['x'].apply(lambda x: custom_round(x, base=10))
+    ygrouped1=yslice1.groupby(['x'],as_index=False).agg({'dr':['mean','std']})
+    ygrouped1.columns=['x','dr','std']
+    y1=ygrouped1.fillna(0) #replace NaNs with 0's
 
-        x1=(-210+y1['x'])*mp
-        dr1=y1['dr']*f1*mp
+    x1=(-210+y1['x'])*PIXEL_SIZE    # Determin where the '210' value comes from. Potentially offset? half of width?
+    dr1=y1['dr']*FRAME_RATE*PIXEL_SIZE      
 
-        fig=plt.figure()
-        ax=fig.add_subplot(111)
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
 
-        ax.scatter(x1,dr1,color='r',label='0.31750 SLPM')
+    ax.scatter(x1,dr1,color='r',label='0.31750 SLPM')
 
-        plt.xlabel('Position',fontsize=20)
-        plt.ylabel('Velocity (mm/s)',fontsize=20)
-        plt.title(location + '\nVelocity vs. Position',fontsize=20)
-        ax.legend()
-        fig.savefig(save_path)
+    plt.xlabel('Position',fontsize=20)
+    plt.ylabel('Velocity (mm/s)',fontsize=20)
+    plt.title(location + ': \nVelocity vs. Position',fontsize=20)
+    ax.legend()
+    fig.savefig(save_path)
+
+
+def vector(raw_data, location, save_path):
+    # Import desired elements
+    raw_data = raw_data.astype({'x':'int','y':'int','dx':'float','dy':'float'})
+    data = raw_data.loc[:,['x','y','dx','dy']]
+
+    # Round values to bin sizes
+    data['x'] = data['x'].apply(lambda x: custom_round(x, base = 50))
+    data['y'] = data['y'].apply(lambda y: custom_round(y, base = 50))
+
+    # Organize data for plot
+    data = data.groupby(['x','y']).mean()
+    data = data.reset_index()
+    
+    # Adjust for pixel size and framerate
+    data.x = (data.x) * PIXEL_SIZE
+    data.y = (data.y * -1 + data.y.values.max()) * PIXEL_SIZE
+    data.dx = data.dx * FRAME_RATE * PIXEL_SIZE
+    data.dy = data.dy * FRAME_RATE * PIXEL_SIZE * -1
+
+    # Create and label plot
+    plt.figure(figsize = (12,10))
+    plt.title(location, fontsize = 45)
+    plt.quiver(data['x'],data['y'],data['dx'],data['dy'])
+    plt.xlabel('x-distance (mm)', fontsize = 30)
+    plt.ylabel('y-distance (mm)', fontsize = 30)
+    
+    # Save Plot
+    try:
+        plt.savefig(save_path)
+    except:
+        print("Unexpected error saving plot:", sys.exc_info())      
+
 
 
 if __name__ == '__main__':
