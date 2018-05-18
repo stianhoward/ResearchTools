f1a851e927ada8c1223d808326fd05a4c9425551
Stian Howard <stianhoward@gmail.com>
2018-05-18 14:20:23 -0600

    Removed comment code and argument parser. Moved operations into main().

--- lc_turbulence/turb.py
+++ lc_turbulence/turb.py
@@ -2,16 +2,12 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-
 # Optionally, tweak styles.
 mpl.rc('figure',  figsize=(10, 6))
 mpl.rc('image', cmap='gray')
-
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series  # for convenience
-
 import pims
 import trackpy as tp
 # from IPython.html.widgets import interact, interactive, fixed
@@ -29,67 +25,33 @@ from scipy.ndimage.interpolation import rotate
 import trackpy.predict
 from scipy.optimize import fmin
 import glob
-import argparse
 import os
 import sys
 
 
 #User options#
+THRESH = 45
 #Folder to analyze
-path = '/media/stian/Evan Dutch/Turbulence/2018-05-17/'
-
-#add in command line arguments for debugging
-parser = argparse.ArgumentParser(description='Fragile program for analyzing and stiching together data for the nozzle experiment')
-
-parser.add_argument('analyze', help = 'do you want to analyze the raw data? true/false', type =int)
+# path = '/media/stian/Evan Dutch/Turbulence/2018-05-17/'
+path = '/home/stian/Desktop/test/'
 
 
-args = parser.parse_args()
-
-if args.analyze==1:
+def main():
     films = glob.glob(path + 'Film[1-9]')
     print(films)
     for film in films:
         film = film + '/'
-
-        print('analyzing ' + film)
         dirnames = glob.glob(film + '[1-9]')
         #should be numbers 1 through 9, one for each region
 
-        def thresh(img):
-            #determine threshold values from imagej
-            img[img<45] = 0
-            img[img>45] = 255
-            return util.img_as_int(img)
-        '''
-        def detect(num, img):
-            label_image,num_regions = skimage.measure.label(img, return_num=True)
-            if num_regions <1:
-                raise ValueError('No Regions')
-                #print num_regions
-            features = pd.DataFrame()
-
-            for region in skimage.measure.regionprops(label_image, intensity_image = img):
-                if region.area <10 or region.area >900000:
-                    continue
-                if region.mean_intensity ==255:
-                    continue
-                if region.eccentricity > .78:
-                    continue
-                features = features.append([{'y':region.centroid[0],
-                                            'x':region.centroid[1],
-                                            'frame':num}])
-            return features
-            '''
-
-        BigData = [] #list to hold data from all the regions
+        # BigData = [] #list to hold data from all the regions
         for number in dirnames:
+            print("Analyzing " + number)
             frames = pims.ImageSequence(number+'//*.bmp', process_func=thresh)
             
             datastorename = film+'data-t'+number.strip()[-1]+'.h5'
             with tp.PandasHDFStoreBig(datastorename) as s:
                 for num, img in enumerate(frames):
-                    print(num)
                     label_image,num_regions = skimage.measure.label(img, return_num=True)
                     if num_regions <1:
                         break
@@ -137,15 +99,15 @@ if args.analyze==1:
                                         }])
 
             data.to_csv(film+'t'+number.strip()[-1]+'valuematrix.csv')
-            BigData.append(data)
-        
-#Now, stich everything together.
+            # BigData.append(data)
 
-#First split everything apart by region (the reason I don't do this before is to maintain a clear distinction between the 'raw' data and the processed data. So if something goes wrong, you can at least look at the raw .csv files.
-if args.analyze == False:
-    #read in csv file names
-    BigData = []
-    filenames = glob.glob('t*[0-9]*.csv')
-    for name in filenames:
-        temp = pd.read_csv(name, sep=',')
-        BigData.append(temp)
+
+def thresh(img):
+    #determine threshold values from imagej
+    img[img<THRESH] = 0
+    img[img>THRESH] = 255
+    return util.img_as_int(img)
+
+
+if __name__ == '__main__':
+    main()
