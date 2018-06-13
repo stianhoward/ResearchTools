8c766f21ee669b0d5444bffb13b30d4e6e8a08bf
Stian Howard <stianhoward@gmail.com>
2018-06-13 16:36:39 -0600

    Remove redundant code

--- turbulence/reynoldsnumber.py
+++ turbulence/reynoldsnumber.py
@@ -4,9 +4,9 @@ Created on Tue Oct 17 12:57:19 2017
 
 @author: edutc
 """
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
 # Program to calculate the reynolds number of 8CB as a function of velocity
@@ -23,10 +23,12 @@ def ReynoldsNumber(v):
        
     visc  = 0.052                #vescocity of 8CB in Pa s
     L     = 0.003556             #width of channel in m
+    L     = 0.00245
     pd    = 1000.0               #density of 8CB in kg m^-3
     Re = v*L*pd/visc
     return Re 
 
+'''
 data=pd.read_csv('C:\\Users\\edutc\\Desktop\\WORK\\racetrack\\data.csv')    #read in racetrack data
 
 data=data/1000
@@ -39,4 +41,4 @@ plt.ylabel('Re')
 
 
 print('In the racetrack experiment, the reynolds number ranged from {:.5f} to {:.5f}'.format(ReynoldVals.min(),ReynoldVals.max()))
-
+'''
