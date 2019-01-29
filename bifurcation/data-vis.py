5f74da3f9f5d734238c11428a0dc2ae9576ea857
Stian Howard <stianhoward@gmail.com>
2019-01-29 11:31:05 -0700

    Added bifurcation analysis script

---
+++ bifurcation/data-vis.py
@@ -0,0 +1,60 @@
+#Program to display a 3D plot of the energy landscape from the PIV results
+
+import numpy as np
+import pandas as pd
+import glob as glob
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import re as re
+import os
+from scipy.interpolate import griddata
+#First, read in the data
+
+data = []
+location = 'back/'
+folder = '2851'
+os.chdir(location + folder + '/')
+
+fileNames = glob.glob("*.txt")
+frameSearch = re.compile(folder + '_(\d{4}).txt')
+#the first four columns are the most important
+cols =['x', 'y', 'u', 'v','SNR','valid','filtered','avg. intensity'] 
+nanTest = '       nan'
+for name in fileNames:
+    temp = pd.read_csv(name,sep='\t',header=0,names = cols,na_values =nanTest)
+    temp['filename'] = name
+    temp['frame'] = int(frameSearch.match(name)[1])
+    data.append(temp)
+
+data = pd.concat(data)
+data = data[data['filtered']==0]
+data['y'] = data['y'].max() - data['y'] + 15
+data['v'] = data['v']
+groups = data.groupby(['x','y'])
+aveData = groups.agg('mean')
+vel = aveData['u']**2+aveData['v']**2
+valueVel = vel[~vel.isnull()]
+#valueVel=valueVel[np.abs(valueVel)<4]
+keys = valueVel.keys()
+pts = np.array([(k[0],k[1]) for k in keys])
+xSpace = np.linspace(0,data['x'].max(),200)
+ySpace = np.linspace(0,data['y'].max(),200)
+velGrid = griddata( (pts[:,0],pts[:,1]), valueVel, (xSpace[None,:],ySpace[:,None]),method='cubic')
+levels = np.linspace(0,4,30)
+CS = plt.contourf(xSpace[::1], ySpace[::-1],velGrid,levels=levels)
+plt.colorbar()
+
+#now, plot this over the frame image
+fig,ax = plt.subplots()
+im = mpl.image.imread(glob.glob('*.bmp')[0])
+idx = pd.IndexSlice
+aveDataFiltered=aveData.loc[keys]
+plt.quiver(aveDataFiltered.index.get_level_values(0),aveDataFiltered.index.get_level_values(1),aveDataFiltered['u'],aveDataFiltered['v'])
+plt.imshow(im,alpha=.5)
+plt.show()
+#now, try to grid this and make a contour plot
+def velGrid(x,y):
+    try:
+        return valueVel[x,y]
+    except KeyError:
+        return 0.
