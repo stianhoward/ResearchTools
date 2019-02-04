16ed0a82dbb0b787b1988943612a0bad26f5c04d
Stian Howard <stianhoward@gmail.com>
2019-02-04 11:33:46 -0700

    Updates to image stitching (now actually work).

--- bifurcation/image_stitching.py
+++ bifurcation/image_stitching.py
@@ -2,23 +2,31 @@ import os
 from PIL import Image
 import numpy as np
 
-front_image_path = '/home/stian/Desktop/Bifurcation/2805/back/2427/2427_'
-back_image_path = '/home/stian/Desktop/Bifurcation/2805/front/2427/2427_'
+front_image_path = '/media/stian/Evan Dutch/Bifurcation/12805/front/2848/2848_'
+back_image_path = '/media/stian/Evan Dutch/Bifurcation/12805/back/2851/2851_'
 
 front_img_px = 1176
 back_img_px = 295
-image_count = 100
+image_count = 1000
 
 for i in range(image_count):
+    print('Stitching image ' + str(i+1) + '/' + str(image_count), end='\r')
     num = ('0000'+ str(i+1))[-4:]
     front_path = front_image_path + num + '.bmp'
     back_path = back_image_path + num + '.bmp'
 
     front_img = Image.open(front_path)
-    front_img = np.array(front_img)[0:800, 0:back_img_px+640]
+    front_img = np.array(front_img)[0:800, front_img_px-740:1280]
     back_img = Image.open(back_path)
-    back_img = np.array(back_img)[0:800, front_img_px-740:1280]
+    back_img = np.array(back_img)[0:800, 0:back_img_px+640]
+    #back_img = np.flip(back_img,0)
 
-    stitched_img = np.concatenate((front_img, back_img), axis=1)
+    stitched_img = np.concatenate((back_img, front_img), axis=1)
     result = Image.fromarray((stitched_img).astype(np.uint8))
-    result.save('/home/stian/Desktop/Bifurcation/2805/stitched/2427_' + num + '.bmp')
+    result.save('/media/stian/Evan Dutch/Bifurcation/12805/stitched/285/' + num + '.bmp')
+
+print()
+
+# back_img = np.flip(back_img, 0)
+# result = Image.fromarray((back_img).astype(np.uint8))
+# result.show()
