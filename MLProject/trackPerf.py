3391fa9d881e8d90860ec2f9c04fb342b8b832d9
Stian Howard <stianhoward@gmail.com>
2019-07-10 16:13:27 -0600

    rms of nearest point is now calculated

--- MLProject/trackPerf.py
+++ MLProject/trackPerf.py
@@ -1,20 +1,20 @@
 import json
-import trackpy
+import trackpy as tp
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import pandas as pd
 import os
 
-def loadJSON(directory):
+def loadJSON(directory,height):
   array = np.empty([0,3])
   files = sorted(glob.glob(os.path.join(directory,'*.json')))
   for j in range(0,len(files)):
     with open(files[j], 'r') as read_file:
       data = json.load(read_file)
-      for i in range(o,len(data)):
+      for i in range(0,len(data)):
         x = (data[i]['bottomright']['x'] + data[i]['topleft']['x'])/2
-        y = (data[i]['bottomright']['y'] + data[i]['topleft']['y'])/2
+        y = height-(data[i]['bottomright']['y'] + data[i]['topleft']['y'])/2
         array = np.vstack([array,[j,x,y]])
 
   array = array.astype(int)
@@ -36,11 +36,33 @@ def loadCSV(directory):
   dataset = pd.DataFrame({'frame':array[:,0],'x':array[:,1],'y':array[:,2]})
   return dataset
   
-def plotXs(data):
-  plt.plot(data.x,data.y,'rx')
+def plotTracks(humanData,mlTracks):
+  for particle in mlTracks['particle'].unique():
+    path = mlTracks[mlTracks.particle == particle]
+    plt.plot(path.x,path.y,'b-')
+  plt.plot(humanData.x,humanData.y,'rx')
+
+def trackPoints(data):
+  t = tp.link_df(data, 15, memory = 10)
+  t1 = tp.filter_stubs(t,70)
+  return t1
+
+def pointDist(point,array):
+  min_dist = np.sqrt((array.x-point.x)**2 + (array.y-point.y)**2)
+  #dist = math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+  return(min_dist.min())
+  
+def calcError(human,ml):
+  dists = np.empty([1])
+  human = human[['x','y']]
+  for i in range(0,len(human.index)):
+    dists = np.append(dists,[pointDist(human.loc[i,:],ml[['x','y']])])
+  print(np.sqrt(np.mean(dists ** 2)))
 
 if __name__ == "__main__":
-  humanSet = loadCSV('/home/stian/Desktop/validation/highC/annotations/csv/')
-  mlSet = loadJSON('/home/stian/Dekstop/validation/highC/Pos0/corrected')
-  plotXs(humanSet)
+  humanSet = loadCSV('/home/stian/Desktop/testVid/r2/MLCSV')
+  mlSet = loadJSON('/home/stian/Desktop/outIMG/',800)
+  tracks = trackPoints(mlSet)
+  plotTracks(humanSet,tracks)
+  calcError(humanSet,mlSet)
   plt.show()
